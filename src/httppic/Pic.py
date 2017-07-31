6be64a06d064d8be4c2b5d6c6e3d949ad2b77c34
yoozoom <yoozoom@126.com>
2017-07-31 09:40:52 +0800

    download pic

--- src/httppic/Pic.py
+++ src/httppic/Pic.py
@@ -1,18 +1,23 @@
 # -*- coding:utf-8 -*-
 # author yuzuo.yz 2017/7/25 21:23
+import re
+from hashlib import md5
 from urllib import urlencode
 
+import os
 import requests
 import json
+
+from bs4 import BeautifulSoup
 
 
 def get_html():
     data = {
         'offset': 0,
         'format': 'json',
-        'keyword': '美女',
+        'keyword': '黑丝诱惑',
         'autoload': 'true',
-        'count': 20,
+        'count': 5,
         'cur_tab': 3
     }
     url = 'https://www.toutiao.com/search_content/'
@@ -41,16 +46,73 @@ def build_url(url, endpoint):
     return "/".join([url, endpoint])
 
 
+def get_art(text):
+    json_data = json.loads(text)
+    data_list = json_data['data']
+    for art_item in data_list:
+        yield art_item['article_url']
+
+
+def get_art_detail(url):
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            return response.text
+    except Exception, e:
+        print "http request error!", e
+        return None
+
+
+def get_art_name_img(html):
+    soup = BeautifulSoup(html, 'lxml')
+    title = soup.select('title')[0].get_text()
+    print title
+    img_pattern = re.compile('var gallery = (.*?);', re.S)
+    rs = re.search(img_pattern, html)
+    if rs:
+        img_list_str = rs.group(1)
+        img_list_json = json.loads(img_list_str)
+        sub_images = img_list_json['sub_images']
+        if sub_images:
+            for sub_images_item in sub_images:
+                yield sub_images_item['url']
+
+
+def get_img_content(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.content
+    except Exception, e:
+        print "http request error!", e
+        return None
+
+
+def download_img(url):
+    content = get_img_content(url)
+    if content:
+        file_name = md5(content).hexdigest() + ".jpg"
+        if not os.path.exists(file_name):
+            with open(file_name, 'wb') as f:
+                print "download..."
+                f.write(content)
+
+
 def print_json(data):
-    print data
     return json.dumps(json.loads(data), indent=4)
 
 
 def main():
     html = get_html()
-    print print_json(html)
+    for art_url in get_art(html):
+        detail_html = get_art_detail(art_url)
+        for img_url in get_art_name_img(detail_html):
+            if img_url:
+                print img_url
+                download_img(img_url)
 
-    # test method
+        # test method
     # print build_url("http://www.baidu.com", "hello")
     # test json
     # test_str = '{"name":"kaka", "age": 10, "fav":{"name":"football","title":"play"}}'
